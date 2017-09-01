eafe4f6adac9058720d2f04980fdf40d785f709c
cash <cash@cashwu.com>
2017-09-01 16:07:21 +0800

    init

--- Kata20170901_SumConsecutives/SumConsecutivesTests.cs
+++ Kata20170901_SumConsecutives/SumConsecutivesTests.cs
@@ -4,7 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Kata20170901_SumConsecutives
 {
     [TestClass]
-    public class UnitTest1
+    public class SumConsecutivesTests
     {
         [TestMethod]
         public void TestMethod1()
