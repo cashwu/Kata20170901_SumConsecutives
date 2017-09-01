99af99914347a6b533e5a04568b0fd405135987b
cash <cash@cashwu.com>
2017-09-01 16:19:11 +0800

    init 1,2,3 test case

--- Kata20170901_SumConsecutives/SumConsecutivesTests.cs
+++ Kata20170901_SumConsecutives/SumConsecutivesTests.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Kata20170901_SumConsecutives
@@ -7,8 +9,24 @@ namespace Kata20170901_SumConsecutives
     public class SumConsecutivesTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void input_1_2_3_should_return_1_2_3()
         {
+            SumConsecutivesShouldBe(new List<int> {1, 2, 3}, new List<int> {1, 2, 3});
+        }
+
+        private static void SumConsecutivesShouldBe(List<int> expected, List<int> list)
+        {
+            var consecutives = new Consecutives();
+            var actual = consecutives.SumConsecutives(list);
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+
+    public class Consecutives
+    {
+        public List<int> SumConsecutives(List<int> list)
+        {
+            return list;
         }
     }
 }
